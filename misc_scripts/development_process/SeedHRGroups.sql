01b07484304cd2b43f6966b01d9e6d6719238ce9
wilkins.92 <wilkins.92@osu.edu>
2012-12-17 14:06:45 -0500

    Local commit to database diagram and Joan's changes.

---
+++ misc_scripts/development_process/SeedHRGroups.sql
@@ -0,0 +1,6 @@
+﻿SELECT DISTINCT d.dept_name, d.deptid, u.id AS user_id, ui.inst_username, u.resource_id
+FROM kmdata.departments d
+INNER JOIN kmdata.user_appointments ua ON d.deptid = ua.department_id
+INNER JOIN kmdata.users u ON ua.user_id = u.id
+INNER JOIN kmdata.user_identifiers ui ON u.id = ui.user_id
+ORDER BY d.dept_name, ui.inst_username;
--- procedures/researchinview/researchinview.insert_unpublished.sql
+++ procedures/researchinview/researchinview.insert_unpublished.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION researchinview.insert_unpublished (
+﻿CREATE OR REPLACE FUNCTION researchinview.insert_unpublished (
    p_IntegrationActivityId VARCHAR(2000),
    p_IntegrationUserId VARCHAR(2000),
    p_IsPublic INTEGER,
