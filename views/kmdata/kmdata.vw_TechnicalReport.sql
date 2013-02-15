57d7a9ff4c49486be84bc1977b6c3dca555117be
Joan Qin <qin.42@osu.edu>
2013-02-15 13:05:06 -0500

    add look up tables into view

--- views/kmdata/kmdata.vw_TechnicalReport.sql
+++ views/kmdata/kmdata.vw_TechnicalReport.sql
@@ -1,7 +1,17 @@
-﻿CREATE OR REPLACE VIEW kmdata.vw_TechnicalReport AS 
+CREATE OR REPLACE VIEW kmdata.vw_TechnicalReport AS 
  SELECT w.id, w.resource_id, w.user_id, w.title, w.author_list, w.editor_list, w.issue, w.percent_authorship, w.review_type_id, w.status_id, 
         w.url, w.volume, w.created_at, w.updated_at, w.work_type_id, w.city, w.state, w.country, w.edition, w.publisher, w.isbn, 
-        w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year
+        w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
+        w.title, w.author_list, w.editor_list, 
+	          w.issue, w.percent_authorship, w.publication_type_id, d.name as technical_report_type, w.report_number,
+	          w.review_type_id, f.name as review_type, w.status_id, e.name as status_type, w.url, w.volume,  w.work_type_id,
+	          w.city, w.state, w.country, w.edition, w.publisher, w.isbn, w.is_review,
+          w.publication_dmy_single_date_id, sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
+   left join kmdata.narratives b on w.resource_id = b.resource_id
+      left join researchinview.riv_technical_report_types d on d.id = w.publication_type_id
+      left join researchinview.riv_publication_statuses e on e.id = w.status_id
+      left join researchinview.activity_import_log c on c.resource_id = w.resource_id
+   left join researchinview.riv_review_types f on f.id = w.review_type_id
   WHERE w.work_type_id = 7;
