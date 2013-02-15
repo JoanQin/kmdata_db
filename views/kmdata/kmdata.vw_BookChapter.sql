b42e3a31c9ea8074af7f2410e4e7770c02e76405
Joan Qin <qin.42@osu.edu>
2013-02-15 13:57:19 -0500

    add look up tables into view

--- views/kmdata/kmdata.vw_BookChapter.sql
+++ views/kmdata/kmdata.vw_BookChapter.sql
@@ -1,9 +1,19 @@
-﻿CREATE OR REPLACE VIEW kmdata.vw_BookChapter AS 
+CREATE OR REPLACE VIEW kmdata.vw_BookChapter AS 
  SELECT w.id, w.resource_id, w.user_id, w.author_list, w.city, w.country, w.edition, w.editor_list, w.percent_authorship, 
         w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
         w.publisher, w.review_type_id, w.state, w.title, w.title_in, w.url, w.volume, 
         w.work_type_id, w.created_at, w.updated_at, w.isbn, w.lccn,
-        w.beginning_page, w.ending_page, w.status_id, w.book_author
+        w.beginning_page, w.ending_page, w.status_id, w.book_author,
+        w.author_list, w.city, w.country, w.edition, w.editor_list, w.percent_authorship, w.publication_dmy_single_date_id,
+	          w.publisher, w.is_review, f.name as review_type, w.review_type_id, w.state, w.title, w.title_in, w.url, w.volume, w.work_type_id,
+	          a.work_type_name, w.isbn, w.lccn,
+	          w.beginning_page, w.ending_page, w.status_id, e.name as publication_status, w.book_author,
+           sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
+   left join kmdata.narratives b on w.resource_id = b.resource_id
+      left join researchinview.activity_import_log c on c.resource_id = w.resource_id
+      left join kmdata.work_types a on a.id = w.work_type_id
+      left join researchinview.riv_publication_statuses e on e.id = w.status_id   
+   left join researchinview.riv_review_types f on f.id = w.review_type_id
   WHERE w.work_type_id = 8;
