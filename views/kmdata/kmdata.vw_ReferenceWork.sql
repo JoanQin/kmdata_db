0132b264aec42f01b8cf9ac5e2f95e80473217bd
Joan Qin <qin.42@osu.edu>
2013-02-15 14:10:04 -0500

    update views

--- views/kmdata/kmdata.vw_ReferenceWork.sql
+++ views/kmdata/kmdata.vw_ReferenceWork.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW kmdata.vw_ReferenceWork AS 
  SELECT w.id, w.resource_id, w.user_id, w.title, w.author_list, w.beginning_page, w.ending_page, w.percent_authorship, 
         w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
-        w.review_type_id, w.status_id, w.url, w.volume, w.created_at, w.updated_at, w.work_type_id, w.city, w.country, w.state, w.isbn,
+        w.review_type_id, w.status_id, w.url, w.volume, w.created_at, w.updated_at, w.work_type_id, a.work_type_name, w.city, w.country, w.state, w.isbn,
         w.author_list as author, w.editor_list as editor, w.publication_title as title_of_publication, 
 	w.title as title_of_entry, w.sub_work_type_id as type_of_work_id, d.name as type_of_work, 
 	w.status_id as publication_status_id, e.name as publication_status,
@@ -14,4 +14,5 @@ CREATE OR REPLACE VIEW kmdata.vw_ReferenceWork AS
    left join researchinview.riv_reference_work_types d on d.id = w.sub_work_type_id
    left join researchinview.riv_publication_statuses e on e.id = w.status_id
    left join researchinview.activity_import_log c on c.resource_id = w.resource_id
+   left join kmdata.work_types a on a.id = w.work_type_id
   WHERE w.work_type_id = 5;
