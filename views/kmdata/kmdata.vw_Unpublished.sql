ee3a661d75faa65ea800e7206d8b9521dab8e3da
Joan Qin <qin.42@osu.edu>
2013-02-15 15:54:37 -0500

    update views

--- views/kmdata/kmdata.vw_Unpublished.sql
+++ views/kmdata/kmdata.vw_Unpublished.sql
@@ -1,7 +1,17 @@
-﻿CREATE OR REPLACE VIEW kmdata.vw_Unpublished AS 
+CREATE OR REPLACE VIEW kmdata.vw_Unpublished AS 
  SELECT w.id, w.resource_id, w.user_id, w.title, w.author_list, w.percent_authorship, 
         w.submission_dmy_single_date_id, sd1.day AS submission_day, sd1.month AS submission_month, sd1.year AS submission_year,
-        w.status_id, w.review_type_id, w.created_at, w.updated_at, w.work_type_id, w.submitted_to
+        w.status_id, w.review_type_id, w.created_at, w.updated_at, w.work_type_id, w.submitted_to,
+        title, author_list, completed,
+	          percent_authorship, submission_dmy_single_date_id, 
+	          review_type_id, f.name as review_type_name, work_type_id, a.work_type_name,
+	          submitted_to, extended_author_list, status_id, e.name as publication_status,
+           sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.submission_dmy_single_date_id = sd1.id
+   left join kmdata.narratives b on w.resource_id = b.resource_id
+      left join researchinview.activity_import_log c on c.resource_id = w.resource_id
+      left join kmdata.work_types a on a.id = w.work_type_id
+      left join researchinview.riv_publication_statuses e on e.id = w.status_id   
+   left join researchinview.riv_review_types f on f.id = w.review_type_id
   WHERE w.work_type_id = 10;
