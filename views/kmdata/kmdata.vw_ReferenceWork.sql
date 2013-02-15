6d7cb448777bdb9f20572374534d07885fbf2a70
Joan Qin <qin.42@osu.edu>
2013-02-15 08:56:06 -0500

    update view

--- views/kmdata/kmdata.vw_ReferenceWork.sql
+++ views/kmdata/kmdata.vw_ReferenceWork.sql
@@ -6,11 +6,12 @@ CREATE OR REPLACE VIEW kmdata.vw_ReferenceWork AS
 	w.title as title_of_entry, w.sub_work_type_id as type_of_work_id, d.name as type_of_work, 
 	w.status_id as publication_status_id, e.name as publication_status,
 	w.is_review as reviewed_item, w.percent_authorship, b.narrative_text as description_of_effort,
-	w.isbn, w.url, b.private_ind as privacy, w.beginning_page, w.ending_page, w.publisher, w.city, w.state, w.country,
+	w.isbn, w.url, b.private_ind as privacy, c.is_public, w.beginning_page, w.ending_page, w.publisher, w.city, w.state, w.country,
 	w.volume, w.edition, sd1.day as published_on_year, sd1.month as published_on_month, sd1.year as published_on_year
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
    left join researchinview.riv_reference_work_types d on d.id = w.sub_work_type_id
    left join researchinview.riv_publication_statuses e on e.id = w.status_id
+   left join researchinview.activity_import_log c on c.resource_id = w.resource_id
   WHERE w.work_type_id = 5;
