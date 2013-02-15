6ec0ad32ed455f6e4c33d04de01b11984c7556ad
Joan Qin <qin.42@osu.edu>
2013-02-15 16:01:31 -0500

    update views

--- views/kmdata/kmdata.vw_Unpublished.sql
+++ views/kmdata/kmdata.vw_Unpublished.sql
@@ -12,6 +12,6 @@ CREATE OR REPLACE VIEW kmdata.vw_Unpublished AS
    left join kmdata.narratives b on w.resource_id = b.resource_id
       left join researchinview.activity_import_log c on c.resource_id = w.resource_id
       left join kmdata.work_types a on a.id = w.work_type_id
-      left join researchinview.riv_publication_statuses e on e.id = w.status_id   
+      left join researchinview.riv_unpublish_submission_status e on e.id = w.status_id   
    left join researchinview.riv_review_types f on f.id = w.review_type_id
   WHERE w.work_type_id = 10;
