865b0f73024194eeec997d432bfb4ccca70466a7
Joan Qin <qin.42@osu.edu>
2013-02-18 12:31:26 -0500

    update views

--- views/kmdata/kmdata.vw_Book.sql
+++ views/kmdata/kmdata.vw_Book.sql
@@ -2,11 +2,9 @@ CREATE OR REPLACE VIEW kmdata.vw_Book AS
  SELECT w.id, w.resource_id, w.user_id, w.author_list, w.city, w.country, w.edition, w.percent_authorship, 
         w.publisher, w.review_type_id, w.state, w.title, w.url, w.volume, w.work_type_id, 
         w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
-        w.created_at, w.updated_at, w.isbn, w.lccn, w.editor_list, w.status_id, w.sub_work_type_id,
-        w.author_list, w.city, w.country, w.edition, w.editor_list, w.percent_authorship, 
-	w.publisher, w.is_review, w.review_type_id, d.name as review_type, w.state, w.title, w.url, w.volume, w.work_type_id, a.work_type_name,
-	w.publication_dmy_single_date_id, 
-           w.isbn, w.lccn, w.status_id, e.name as publication_status, w.sub_work_type_id, f.name, sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
+        w.created_at, w.updated_at, w.isbn, w.lccn, w.editor_list, w.status_id, w.sub_work_type_id,         
+	 w.is_review, d.name as review_type,  a.work_type_name,	
+           e.name as publication_status,  f.name,  b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
