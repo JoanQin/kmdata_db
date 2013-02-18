dfc5bd7f46e38cf002fd66c82398fc4076085176
Joan Qin <qin.42@osu.edu>
2013-02-18 13:08:35 -0500

    update views

--- views/kmdata/kmdata_vw_Press.sql
+++ views/kmdata/kmdata_vw_Press.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE VIEW kmdata.vw_Press AS 
+CREATE OR REPLACE VIEW kmdata.vw_Press AS 
  SELECT   w.id, w.resource_id, w.user_id,   w.article_title, w.author_list, w.is_review,
           w.issn, w.issue, w.beginning_page, w.ending_page, w.percent_authorship, w.frequency_of_publication, g.name as publication_frequency,
           w.frequency_of_publication_other, w.publication_title, w.publication_type_id, h.name as press_publication_type,
@@ -8,7 +8,7 @@
           w.series_frequency, w.series_frequency_other, w.series_ongoing, 
           w.series_started_on_dmy_single_date_id, sd3.year as series_start_year, sd3.month as series_start_month, sd3.day as series_start_day,
           w.title, w.single_or_series, w.url, w.volume, w.created_at, w.updated_at,  w.work_type_id, a.work_type_name, 
-           sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
+           sd1.year as publicatin_year, sd1.month as publication_month, sd1.day as publication_day, b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
