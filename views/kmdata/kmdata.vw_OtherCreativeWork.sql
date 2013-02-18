cdf875387e3a8150c7aadb9c085dac64d81f5a0a
Joan Qin <qin.42@osu.edu>
2013-02-18 15:58:48 -0500

    update views

--- views/kmdata/kmdata.vw_OtherCreativeWork.sql
+++ views/kmdata/kmdata.vw_OtherCreativeWork.sql
@@ -1,10 +1,11 @@
 CREATE OR REPLACE VIEW kmdata.vw_OtherCreativeWork AS 
- SELECT w.id, w.resource_id, w.user_id, w.author_list, w.format, w.forthcoming_id, a.name as forthcoming, w.percent_authorship, w.role_designator, 
-        w.sponsor, w.title, w.url, 
-        w.creation_dmy_single_date_id, sd.day AS creation_day, sd.month AS creation_month, sd.year AS creation_year,
-        w.presentation_dmy_range_date_id, rd.start_day AS presentation_start_day, rd.start_month AS presentation_start_month, rd.start_year AS presentation_start_year,
-        rd.end_day AS presentation_end_day, rd.end_month AS presentation_end_month, rd.end_year AS presentation_end_year,
-        w.created_at, w.updated_at, w.work_type_id, b.work_type_name,
+SELECT w.id, w.resource_id, w.user_id, w.author_list, w.format, w.forthcoming_id, w.percent_authorship, w.role_designator, w.sponsor, w.title, w.url, 
+w.creation_dmy_single_date_id, sd.day AS creation_day, sd.month AS creation_month, sd.year AS creation_year, 
+w.presentation_dmy_range_date_id, rd.start_day AS presentation_start_day, rd.start_month AS presentation_start_month, 
+rd.start_year AS presentation_start_year, rd.end_day AS presentation_end_day, rd.end_month AS presentation_end_month, 
+rd.end_year AS presentation_end_year, w.created_at, w.updated_at, w.work_type_id,
+ a.name as forthcoming,            
+         b.work_type_name,
         w.sub_work_type_other, w.completed, d.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd ON w.creation_dmy_single_date_id = sd.id
