a68ca0eecfcad3786abeeffa135e392ac844f322
Joan Qin <qin.42@osu.edu>
2013-02-18 14:04:13 -0500

    update views

--- views/kmdata/kmdata.vw_Audiovisual.sql
+++ views/kmdata/kmdata.vw_Audiovisual.sql
@@ -6,13 +6,9 @@ CREATE OR REPLACE VIEW kmdata.vw_Audiovisual AS
         w.presentation_dmy_range_date_id, sd2.day AS presentation_day, sd2.month AS presentation_month, sd2.year AS presentation_year,
         w.title, w.url, 
         w.created_at, w.updated_at, w.work_type_id,
-        city, country, director, distributor, format,
-	          artist, network, percent_authorship, role_designator, sponsor, state, 
-	          broadcast_dmy_single_date_id,
-	          presentation_dmy_range_date_id, sd2.month, sd2.year
-	          title, url,  work_type_id, a.work_type_name,
-	          completed, producer, sub_work_type_id, sub_work_type_other, e.name as audiovisual_work_name,
-           sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
+	           a.work_type_name,
+	         w.completed, w.producer, w.sub_work_type_id, w.sub_work_type_other, e.name as audiovisual_work_name,
+            b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.broadcast_dmy_single_date_id = sd1.id
    LEFT JOIN kmdata.dmy_single_dates sd2 ON w.presentation_dmy_range_date_id = sd2.id
