7f5026fb80874621d0ad68e46a0141c2a8820908
Joan Qin <qin.42@osu.edu>
2013-02-18 13:12:38 -0500

    update views

--- views/kmdata/kmdata.vw_Journal.sql
+++ views/kmdata/kmdata.vw_Journal.sql
@@ -4,12 +4,10 @@ CREATE OR REPLACE VIEW kmdata.vw_Journal AS
         w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
         w.review_type_id, w.status_id, w.url, w.volume, w.created_at, w.updated_at, w.work_type_id, w.citation_count,
         COALESCE(w.extended_author_list, w.author_list) AS extended_author_list,
-        article_title, journal_article_type_id, g.name as journal_article_type_name, author_list, 
-	          issn, impact_factor, issue, journal_title, beginning_page, ending_page, percent_authorship, 
-	          publication_dmy_single_date_id, extended_author_list, is_review,
-	          review_type_id, f.name as review_type_name, status_id, e.name as publication_status, url, volume,  work_type_id, a.work_type_name,
-	          citation_count,
-           sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
+         g.name as journal_article_type_name,  	          
+	         w.is_review,
+	         f.name as review_type_name,  e.name as publication_status,   a.work_type_name,	         
+           b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
