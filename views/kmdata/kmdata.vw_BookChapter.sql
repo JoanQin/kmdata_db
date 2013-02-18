e919e39c7fc639321c2865d88ae335144ef65258
Joan Qin <qin.42@osu.edu>
2013-02-18 16:09:32 -0500

    update views

--- views/kmdata/kmdata.vw_BookChapter.sql
+++ views/kmdata/kmdata.vw_BookChapter.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE VIEW kmdata.vw_BookChapter AS 
- SELECT w.id, w.resource_id, w.user_id, w.author_list, w.city, w.country, w.edition, w.editor_list, w.percent_authorship, 
-        w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
-        w.publisher, w.is_review, w.review_type_id, w.state, w.title, w.title_in, w.url, w.volume, 
-        w.work_type_id, w.created_at, w.updated_at, w.isbn, w.lccn,
-        w.beginning_page, w.ending_page, w.status_id, w.book_author,        
+SELECT w.id, w.resource_id, w.user_id, w.author_list, w.city, w.country, w.edition, w.editor_list, w.percent_authorship, 
+w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year, 
+w.publisher, w.review_type_id, w.state, w.title, w.title_in, w.url, w.volume, w.work_type_id, w.created_at, w.updated_at, 
+w.isbn, w.lccn, w.beginning_page, w.ending_page, w.status_id, w.book_author,       
+        w.is_review,                
 	          f.name as review_type, 
 	          a.work_type_name,  e.name as publication_status, 
             b.narrative_text, c.is_public
