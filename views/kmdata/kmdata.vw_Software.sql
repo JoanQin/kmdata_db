c81f6125385da7b33dd317d4fd0664e134eae9f2
Joan Qin <qin.42@osu.edu>
2013-02-18 15:47:55 -0500

    update views

--- views/kmdata/kmdata.vw_Software.sql
+++ views/kmdata/kmdata.vw_Software.sql
@@ -1,8 +1,10 @@
 CREATE OR REPLACE VIEW kmdata.vw_Software AS 
-SELECT w.id, w.resource_id, w.user_id, w.author_list, w.sponsor, w.percent_authorship, w.publisher, w.role_designator, w.title, w.medium, w.url, w.edition, 
-        w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
-        w.last_update_dmy_single_date_id, sd2.day AS last_update_day, sd2.month AS last_update_month, sd2.year AS last_update_year,
-        w.created_at, w.updated_at, w.work_type_id, a.work_type_name, w.distributor, w.sub_work_type_id, b.name as type_of_work, w.sub_work_type_other,
+ 
+ SELECT w.id, w.resource_id, w.user_id, w.author_list, w.sponsor, w.percent_authorship, w.publisher, w.role_designator, w.title, w.medium, w.url, w.edition, 
+ w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year, 
+ w.last_update_dmy_single_date_id, sd2.day AS last_update_day, sd2.month AS last_update_month, sd2.year AS last_update_year, 
+ w.created_at, w.updated_at, w.work_type_id, w.distributor, w.sub_work_type_id, w.sub_work_type_other,   
+       a.work_type_name,  b.name as type_of_work, 
         c.narrative_text, d.is_public        
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
