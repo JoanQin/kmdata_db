5c185b1969c478ff0aea90759f3c1bb7998608d2
Joan Qin <qin.42@osu.edu>
2013-02-18 12:35:20 -0500

    update views

--- views/kmdata/kmdata.vw_TechnicalReport.sql
+++ views/kmdata/kmdata.vw_TechnicalReport.sql
@@ -1,12 +1,11 @@
 CREATE OR REPLACE VIEW kmdata.vw_TechnicalReport AS 
  SELECT w.id, w.resource_id, w.user_id, w.title, w.author_list, w.editor_list, w.issue, w.percent_authorship, w.review_type_id, w.status_id, 
         w.url, w.volume, w.created_at, w.updated_at, w.work_type_id, w.city, w.state, w.country, w.edition, w.publisher, w.isbn, 
-        w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
-        w.title, w.author_list, w.editor_list, 
-	          w.issue, w.percent_authorship, w.publication_type_id, d.name as technical_report_type, w.report_number,
-	          w.review_type_id, f.name as review_type, w.status_id, e.name as status_type, w.url, w.volume,  w.work_type_id, a.work_type_name,
-	          w.city, w.state, w.country, w.edition, w.publisher, w.isbn, w.is_review,
-          w.publication_dmy_single_date_id, sd1.year, sd1.month, sd1.day, b.narrative_text, c.is_public
+        w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,      
+	            w.publication_type_id, d.name as technical_report_type, w.report_number,
+	           f.name as review_type,  e.name as status_type,    a.work_type_name,
+	               w.is_review,
+           b.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
