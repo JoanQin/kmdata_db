9cc57776e54612316a78fb4c954ebe3b39d20a1c
Joan Qin <qin.42@osu.edu>
2013-02-18 14:54:32 -0500

    update views

--- views/kmdata/kmdata.vw_OtherCreativeWork.sql
+++ views/kmdata/kmdata.vw_OtherCreativeWork.sql
@@ -5,10 +5,12 @@ CREATE OR REPLACE VIEW kmdata.vw_OtherCreativeWork AS
         w.presentation_dmy_range_date_id, rd.start_day AS presentation_start_day, rd.start_month AS presentation_start_month, rd.start_year AS presentation_start_year,
         rd.end_day AS presentation_end_day, rd.end_month AS presentation_end_month, rd.end_year AS presentation_end_year,
         w.created_at, w.updated_at, w.work_type_id, b.work_type_name,
-        w.sub_work_type_other, w.completed
+        w.sub_work_type_other, w.completed, d.narrative_text, c.is_public
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd ON w.creation_dmy_single_date_id = sd.id
    LEFT JOIN kmdata.dmy_range_dates rd ON w.presentation_dmy_range_date_id = rd.id
    left join researchinview.riv_forthcoming a on a.id = w.forthcoming_id
    left join kmdata.work_types b on b.id = w.work_type_id
+   left join kmdata.narratives d on w.resource_id = d.resource_id
+         left join researchinview.activity_import_log c on c.resource_id = w.resource_id
   WHERE w.work_type_id = 23;
