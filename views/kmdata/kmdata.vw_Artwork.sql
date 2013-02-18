898a6ed16bd3760953c7def6c790a419e1c99e62
Joan Qin <qin.42@osu.edu>
2013-02-18 16:14:18 -0500

    update views

--- views/kmdata/kmdata.vw_Artwork.sql
+++ views/kmdata/kmdata.vw_Artwork.sql
@@ -1,11 +1,9 @@
 CREATE OR REPLACE VIEW kmdata.vw_Artwork AS 
- SELECT w.id, w.resource_id, w.user_id, w.artist, w.city, w.country, w.curator, 
-        w.dimensions, w.exhibit_title, w.juried_ind, w.medium, w.percent_authorship, 
-        w.exhibition_type_id, w.sponsor, w.state, w.title, w.url, w.venue, 
-        w.creation_dmy_single_date_id, sd.day AS creation_day, sd.month AS creation_month, sd.year AS creation_year,
-        w.exhibit_dmy_range_date_id, rd.start_day AS exhibit_start_day, rd.start_month AS exhibit_start_month, rd.start_year AS exhibit_start_year, 
-        rd.end_day AS exhibit_end_day, rd.end_month AS exhibit_end_month, rd.end_year AS exhibit_end_year,
-        w.created_at, w.updated_at, w.work_type_id,
+SELECT w.id, w.resource_id, w.user_id, w.artist, w.city, w.country, w.curator, w.dimensions, w.exhibit_title, w.juried_ind, w.medium, 
+w.percent_authorship, w.exhibition_type_id, w.sponsor, w.state, w.title, w.url, w.venue, 
+w.creation_dmy_single_date_id, sd.day AS creation_day, sd.month AS creation_month, sd.year AS creation_year, 
+w.exhibit_dmy_range_date_id, rd.start_day AS exhibit_start_day, rd.start_month AS exhibit_start_month, rd.start_year AS exhibit_start_year, 
+rd.end_day AS exhibit_end_day, rd.end_month AS exhibit_end_month, rd.end_year AS exhibit_end_year, w.created_at, w.updated_at, w.work_type_id,       
 	    a.work_type_name, w.completed, w.ongoing, w.other_artist, w.solo, f.name as solo_name, w.sub_work_type_id, e.name as work_type_name,
            b.narrative_text, c.is_public
    FROM kmdata.works w
