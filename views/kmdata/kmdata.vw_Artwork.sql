0f1c08ea286ecad80b30c5c8775f7a9ee7563231
Joan Qin <qin.42@osu.edu>
2013-02-20 12:43:53 -0500

    update views

--- views/kmdata/kmdata.vw_Artwork.sql
+++ views/kmdata/kmdata.vw_Artwork.sql
@@ -5,7 +5,7 @@ w.creation_dmy_single_date_id, sd.day AS creation_day, sd.month AS creation_mont
 w.exhibit_dmy_range_date_id, rd.start_day AS exhibit_start_day, rd.start_month AS exhibit_start_month, rd.start_year AS exhibit_start_year, 
 rd.end_day AS exhibit_end_day, rd.end_month AS exhibit_end_month, rd.end_year AS exhibit_end_year, w.created_at, w.updated_at, w.work_type_id,       
 	    a.work_type_name, w.completed, w.ongoing, w.other_artist, w.solo, f.name as solo_name, w.sub_work_type_id, e.name as artwork_type_name,
-           b.narrative_text, c.is_public
+           b.narrative_text, c.is_public, lo.is_active
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd ON w.creation_dmy_single_date_id = sd.id
       left join kmdata.narratives b on w.resource_id = b.resource_id
@@ -14,4 +14,5 @@ rd.end_day AS exhibit_end_day, rd.end_month AS exhibit_end_month, rd.end_year AS
          left join researchinview.riv_artwork_types e on e.id = w.sub_work_type_id  
          left join researchinview.riv_artwork_solo_group f on f.id = cast(w.solo as int)
   LEFT JOIN kmdata.dmy_range_dates rd ON w.exhibit_dmy_range_date_id = rd.id
+    left join researchinview.activity_import_log lo on w.resource_id = lo.resource_id
   WHERE w.work_type_id = 16;
