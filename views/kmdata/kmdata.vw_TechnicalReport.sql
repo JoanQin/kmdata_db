1ef31ab2faa2c2c52e97aa085c2547baf0db7c11
Joan Qin <qin.42@osu.edu>
2013-02-20 12:51:06 -0500

    update views

--- views/kmdata/kmdata.vw_TechnicalReport.sql
+++ views/kmdata/kmdata.vw_TechnicalReport.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE VIEW kmdata.vw_TechnicalReport AS
 	            w.publication_type_id, d.name as technical_report_type, w.report_number,
 	           f.name as review_type,  e.name as status_type,    a.work_type_name,
 	               w.is_review,
-           b.narrative_text, c.is_public, lo.is_active
+           b.narrative_text, c.is_public, c.is_active
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
@@ -14,5 +14,4 @@ CREATE OR REPLACE VIEW kmdata.vw_TechnicalReport AS
       left join researchinview.activity_import_log c on c.resource_id = w.resource_id
    left join researchinview.riv_review_types f on f.id = w.review_type_id
       left join kmdata.work_types a on a.id = w.work_type_id
-       left join researchinview.activity_import_log lo on w.resource_id = lo.resource_id
   WHERE w.work_type_id = 7;
