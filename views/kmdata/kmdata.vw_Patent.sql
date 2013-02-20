7741418ecba96b4f3f56fe75e9acab5d886c3ace
Joan Qin <qin.42@osu.edu>
2013-02-20 12:57:06 -0500

    update views

--- views/kmdata/kmdata.vw_Patent.sql
+++ views/kmdata/kmdata.vw_Patent.sql
@@ -4,10 +4,9 @@ CREATE OR REPLACE VIEW kmdata.vw_Patent AS
         a.work_type_name, 
          works.completed, works.issuing_organization, works.application_number, works.attorney_agent, works.patent_assignee, 
          works.patent_class, works.document_number,
-         b.narrative_text, c.is_public, lo.is_active
+         b.narrative_text, c.is_public, c.is_active
    FROM kmdata.works 
    left join kmdata.work_types a on works.work_type_id = a.id
    left join kmdata.narratives b on works.resource_id = b.resource_id
-         left join researchinview.activity_import_log c on c.resource_id = works.resource_id
-          left join researchinview.activity_import_log lo on works.resource_id = lo.resource_id
+         left join researchinview.activity_import_log c on c.resource_id = works.resource_id          
   WHERE works.work_type_id = 19 ;
