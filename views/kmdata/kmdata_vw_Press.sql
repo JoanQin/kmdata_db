cd23653de1fa9f900c12693c036310f5af20fd18
Joan Qin <qin.42@osu.edu>
2013-02-20 12:53:15 -0500

    update views

--- views/kmdata/kmdata_vw_Press.sql
+++ views/kmdata/kmdata_vw_Press.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE VIEW kmdata.vw_Press AS
           w.series_frequency, w.series_frequency_other, w.series_ongoing, 
           w.series_started_on_dmy_single_date_id, sd3.year as series_start_year, sd3.month as series_start_month, sd3.day as series_start_day,
           w.title, w.single_or_series, w.url, w.volume, w.created_at, w.updated_at,  w.work_type_id, a.work_type_name, 
-           sd1.year as publicatin_year, sd1.month as publication_month, sd1.day as publication_day, b.narrative_text, c.is_public,  lo.is_active
+           sd1.year as publicatin_year, sd1.month as publication_month, sd1.day as publication_day, b.narrative_text, c.is_public,  c.is_active
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
@@ -19,5 +19,4 @@ CREATE OR REPLACE VIEW kmdata.vw_Press AS
    left join researchinview.riv_press_publication_types h on h.id = w.publication_type_id
    LEFT JOIN kmdata.dmy_single_dates sd2 ON w.series_ended_on_dmy_single_date_id = sd2.id
    LEFT JOIN kmdata.dmy_single_dates sd3 ON w.series_started_on_dmy_single_date_id = sd3.id
-      left join researchinview.activity_import_log lo on w.resource_id = lo.resource_id
   WHERE w.work_type_id = 54;
