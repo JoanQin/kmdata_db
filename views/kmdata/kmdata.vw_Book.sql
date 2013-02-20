1fec99eea17c700be50b497cec9021e6c93a5e3f
Joan Qin <qin.42@osu.edu>
2013-02-20 12:50:33 -0500

    update views

--- views/kmdata/kmdata.vw_Book.sql
+++ views/kmdata/kmdata.vw_Book.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE VIEW kmdata.vw_Book AS
         w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
         w.created_at, w.updated_at, w.isbn, w.lccn, w.editor_list, w.status_id, w.sub_work_type_id,         
 	 w.is_review, d.name as review_type,  a.work_type_name,	
-           e.name as publication_status,  f.name,  b.narrative_text, c.is_public, lo.is_active
+           e.name as publication_status,  f.name,  b.narrative_text, c.is_public, c.is_active
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    left join kmdata.narratives b on w.resource_id = b.resource_id
@@ -13,5 +13,4 @@ CREATE OR REPLACE VIEW kmdata.vw_Book AS
       left join researchinview.activity_import_log c on c.resource_id = w.resource_id
    left join researchinview.riv_conference_work_types f on f.id = w.sub_work_type_id
    left join kmdata.work_types a on a.id = w.work_type_id
-   left join researchinview.activity_import_log lo on w.resource_id = lo.resource_id
   WHERE w.work_type_id = 6;
