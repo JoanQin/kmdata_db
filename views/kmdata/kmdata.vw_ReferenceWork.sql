1a7db7144ac863677befeac229cc28d095ecce82
Joan Qin <qin.42@osu.edu>
2013-02-27 10:15:00 -0500

    update views

--- views/kmdata/kmdata.vw_ReferenceWork.sql
+++ views/kmdata/kmdata.vw_ReferenceWork.sql
@@ -11,7 +11,8 @@ w.review_type_id, w.status_id, w.url, w.volume, w.created_at, w.updated_at, w.wo
 	 w.edition, g.name as review_type_name, c.is_active
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
-   left join kmdata.narratives b on w.resource_id = b.resource_id
+   LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
+      LEFT JOIN kmdata.narratives b on wn.narrative_id = b.id
    left join researchinview.riv_reference_work_types d on d.id = w.sub_work_type_id
    left join researchinview.riv_publication_statuses e on e.id = w.status_id
    left join researchinview.activity_import_log c on c.resource_id = w.resource_id
