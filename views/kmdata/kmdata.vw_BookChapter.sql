59779755877309463e6534de22b6caee4039603d
Joan Qin <qin.42@osu.edu>
2013-02-28 16:23:02 -0500

    update views

--- views/kmdata/kmdata.vw_BookChapter.sql
+++ views/kmdata/kmdata.vw_BookChapter.sql
@@ -6,7 +6,7 @@ w.isbn, w.lccn, w.beginning_page, w.ending_page, w.status_id, w.book_author,
         w.is_review,                
 	          f.name as review_type, 
 	          a.work_type_name,  e.name as publication_status, 
-            b.narrative_text, c.is_public, c.is_active
+            b.narrative_text, c.is_public, c.is_active, yn.name as review_val
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
@@ -15,5 +15,5 @@ w.isbn, w.lccn, w.beginning_page, w.ending_page, w.status_id, w.book_author,
       left join kmdata.work_types a on a.id = w.work_type_id
       left join researchinview.riv_publication_statuses e on e.id = w.status_id   
    left join researchinview.riv_review_types f on f.id = w.review_type_id
-
+left join researchinview.riv_yes_no yn on yn.value = cast(w.is_review as int)
   WHERE w.work_type_id = 8;
