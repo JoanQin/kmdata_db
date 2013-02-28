8962db32278740da8483002200540377b50488fa
Joan Qin <qin.42@osu.edu>
2013-02-28 17:03:40 -0500

    update views

--- views/kmdata/kmdata.vw_ClinicalService.sql
+++ views/kmdata/kmdata.vw_ClinicalService.sql
@@ -1,5 +1,13 @@
-﻿CREATE OR REPLACE VIEW kmdata.vw_ClinicalService AS
-SELECT id, user_id, clinical_service_type_id, start_date, end_date, 
-       title, duration, location, department, description, output_text, 
-       resource_id, created_at, updated_at, url
-  FROM clinical_service;
+CREATE OR REPLACE VIEW kmdata.vw_ClinicalService AS
+SELECT a.id, a.user_id, a.clinical_service_type_id,  a.start_date, a.end_date, 
+       a.title, a.duration, a.location, a.department, a.description, a.output_text, 
+       a.resource_id, a.created_at, a.updated_at, a.url, a.clinical_hours_per,  d.name as clinical_hour_val,               
+       a.individuals_served, a.ongoing, yn.name as ongoing_val, a.role, c.name as role_val,
+       a.role_other, a.total_hours_teaching_clinic, e.name as cip_val,
+       b.name as service_type_val
+  FROM kmdata.clinical_service a
+  left join researchinview.riv_clinical_service_types b on a.clinical_service_type_id = b.id
+  left join researchinview.riv_yes_no yn on yn.value = cast(a.ongoing as int)
+  left join researchinview.riv_clinical_roles c on c.id = cast(a.role as int)
+  left join researchinview.riv_clinical_hours_freq d on d.id = cast(a.clinical_hours_per as int)
+  left join researchinview.riv_cip_lookup e on e.value = a.department;
