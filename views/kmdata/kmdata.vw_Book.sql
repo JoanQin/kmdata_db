aac30978a50dcf24916004affb8d6f4bebd4546b
Joan Qin <qin.42@osu.edu>
2013-02-28 16:17:39 -0500

    update views

--- views/kmdata/kmdata.vw_Book.sql
+++ views/kmdata/kmdata.vw_Book.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE VIEW kmdata.vw_Book AS
         w.publication_dmy_single_date_id, sd1.day AS publication_day, sd1.month AS publication_month, sd1.year AS publication_year,
         w.created_at, w.updated_at, w.isbn, w.lccn, w.editor_list, w.status_id, w.sub_work_type_id,         
 	 w.is_review, d.name as review_type,  a.work_type_name,	
-           e.name as publication_status,  f.name,  b.narrative_text, c.is_public, c.is_active
+           e.name as publication_status,  f.name,  b.narrative_text, c.is_public, c.is_active, yn.name as review_val
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
@@ -14,4 +14,5 @@ CREATE OR REPLACE VIEW kmdata.vw_Book AS
       left join researchinview.activity_import_log c on c.resource_id = w.resource_id
    left join researchinview.riv_conference_work_types f on f.id = w.sub_work_type_id
    left join kmdata.work_types a on a.id = w.work_type_id
+   left join researchinview.riv_yes_no yn on yn.value = cast(w.is_review as int)
   WHERE w.work_type_id = 6;
