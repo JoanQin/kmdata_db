ab958987a1f5e3252a591c68f348063eee30802f
Joan Qin <qin.42@osu.edu>
2013-03-01 09:24:26 -0500

    update views

--- views/kmdata/kmdata.vw_Advising.sql
+++ views/kmdata/kmdata.vw_Advising.sql
@@ -5,9 +5,11 @@ SELECT adv.id, adv.user_id, adv.institution_id, ins.name AS inst_name, ins.colle
        adv.title, adv.advisee_current_position, adv.notes, adv.output_text, adv.graduate_completed, 
        adv.table_type, adv.resource_id, adv.created_at, adv.updated_at,          
        adv.description_of_effort, b.name as rolename, c.name as levelname, adv.ongoing, e.name as ongoing_ind,
-       adv.role_other, adv.type_of_group, d.name as grouptype, adv.type_of_group_other, adv.url
+       adv.role_other, adv.type_of_group, d.name as grouptype, adv.type_of_group_other, adv.url, 
+       al.is_public, al.is_active
   FROM kmdata.advising adv
   left JOIN kmdata.institutions ins ON adv.institution_id = ins.id
+  left join researchinview.activity_import_log al on al.resource_id = adv.resource_id
   left join researchinview.riv_advising_student_group_roles b on b.id = adv.role_id
   left join researchinview.riv_advising_student_group_academic_levels c on c.id = adv.level_id
   left join researchinview.riv_advising_student_group_type d on d.id = cast(adv.type_of_group as int)
