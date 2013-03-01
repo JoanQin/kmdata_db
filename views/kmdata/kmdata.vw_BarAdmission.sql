ba3c86531f57cf91a873ec8551c3bdb96707f777
Joan Qin <qin.42@osu.edu>
2013-03-01 09:39:03 -0500

    update views

--- views/kmdata/kmdata.vw_BarAdmission.sql
+++ views/kmdata/kmdata.vw_BarAdmission.sql
@@ -1,6 +1,8 @@
 create or replace view vw_baradmission as 
 SELECT a.id, a.resource_id, a.user_id, a.active, 
 case when a.active = '1' then 'Active' when a.active = '2' then 'Inactive' else null end as active_val,
-a.admitted_on_dmy_single_date_id, b.month, b.year, a.country, a.state, a.created_at, a.updated_at
+a.admitted_on_dmy_single_date_id, b.month, b.year, a.country, a.state, a.created_at, a.updated_at,
+al.is_public, al.is_active
    FROM kmdata.bar_admission a
+   left join researchinview.activity_import_log al on al.resource_id = a.resource_id
    left join kmdata.dmy_single_dates b on a.admitted_on_dmy_single_date_id = b.id
