d547279ca572645088b6674237ad28a60993f277
Joan Qin <qin.42@osu.edu>
2013-03-01 09:55:43 -0500

    update views

--- views/kmdata/kmdata.vw_ClinicalService.sql
+++ views/kmdata/kmdata.vw_ClinicalService.sql
@@ -4,8 +4,9 @@ SELECT a.id, a.user_id, a.clinical_service_type_id,  a.start_date, a.end_date,
        a.resource_id, a.created_at, a.updated_at, a.url, a.clinical_hours_per,  d.name as clinical_hour_val,               
        a.individuals_served, a.ongoing, yn.name as ongoing_val, a.role, c.name as role_val,
        a.role_other, a.total_hours_teaching_clinic, e.name as cip_val,
-       b.name as service_type_val
+       b.name as service_type_val, al.is_public, al.is_active
   FROM kmdata.clinical_service a
+  left join researchinview.activity_import_log al on al.resource_id = a.resource_id
   left join researchinview.riv_clinical_service_types b on a.clinical_service_type_id = b.id
   left join researchinview.riv_yes_no yn on yn.value = cast(a.ongoing as int)
   left join researchinview.riv_clinical_roles c on c.id = cast(a.role as int)
