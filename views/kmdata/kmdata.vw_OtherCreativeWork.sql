2d3e19629dd155019636aa6f16ab91a230f6f596
Joan Qin <qin.42@osu.edu>
2013-03-04 15:15:51 -0500

    Signed-off-by: Joan Qin <qin.42@osu.edu>

--- views/kmdata/kmdata.vw_OtherCreativeWork.sql
+++ views/kmdata/kmdata.vw_OtherCreativeWork.sql
@@ -6,7 +6,7 @@ rd.start_year AS presentation_start_year, rd.end_day AS presentation_end_day, rd
 rd.end_year AS presentation_end_year, w.created_at, w.updated_at, w.work_type_id,
  a.name as forthcoming,            
          b.work_type_name,
-        w.sub_work_type_other, w.completed, d.narrative_text, c.is_public, c.is_active
+        w.sub_work_type_other, w.completed, d.narrative_text, c.is_public, c.is_active, yn.name as completed_val
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd ON w.creation_dmy_single_date_id = sd.id
    LEFT JOIN kmdata.dmy_range_dates rd ON w.presentation_dmy_range_date_id = rd.id
@@ -15,4 +15,5 @@ rd.end_year AS presentation_end_year, w.created_at, w.updated_at, w.work_type_id
    LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
       LEFT JOIN kmdata.narratives d on wn.narrative_id = d.id
          left join researchinview.activity_import_log c on c.resource_id = w.resource_id
+      left join researchinview.riv_yes_no yn on yn.value = cast(w.completed as int)   
   WHERE w.work_type_id = 23;
