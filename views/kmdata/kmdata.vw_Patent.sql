c8c0f35cc68f040449b3b07af8d758ed27e53a91
Joan Qin <qin.42@osu.edu>
2013-03-04 15:59:51 -0500

    update views

--- views/kmdata/kmdata.vw_Patent.sql
+++ views/kmdata/kmdata.vw_Patent.sql
@@ -4,10 +4,11 @@ CREATE OR REPLACE VIEW kmdata.vw_Patent AS
         a.work_type_name, 
          works.completed, works.issuing_organization, works.application_number, works.attorney_agent, works.patent_assignee, 
          works.patent_class, works.document_number,
-         b.narrative_text, c.is_public, c.is_active
+         b.narrative_text, c.is_public, c.is_active, d.name as completed_val
    FROM kmdata.works 
    left join kmdata.work_types a on works.work_type_id = a.id
-   LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
-      LEFT JOIN kmdata.narratives b on wn.narrative_id = b.id
-         left join researchinview.activity_import_log c on c.resource_id = works.resource_id          
+   LEFT JOIN kmdata.work_narratives wn ON works.id = wn.work_id
+   LEFT JOIN kmdata.narratives b on wn.narrative_id = b.id
+   left join researchinview.activity_import_log c on c.resource_id = works.resource_id     
+   left join researchinview.riv_yes_no_inprocess d on d.value = cast(works.completed as int)              
   WHERE works.work_type_id = 19 ;
