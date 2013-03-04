b4cf237335e0ee54765f1d7113c73c5a652a5ce3
Joan Qin <qin.42@osu.edu>
2013-03-04 16:40:13 -0500

    update views

--- views/kmdata/kmdata_vw_Press.sql
+++ views/kmdata/kmdata_vw_Press.sql
@@ -9,7 +9,8 @@ CREATE OR REPLACE VIEW kmdata.vw_Press AS
           w.series_started_on_dmy_single_date_id, sd3.year as series_start_year, sd3.month as series_start_month, sd3.day as series_start_day,
           w.title, w.single_or_series, w.url, w.volume, w.created_at, w.updated_at,  w.work_type_id, a.work_type_name, 
            sd1.year as publicatin_year, sd1.month as publication_month, sd1.day as publication_day, b.narrative_text, c.is_public,  c.is_active,
-           sf.name as series_frequency_name
+           sf.name as series_frequency_name, yn1.name as single_series_val, yn2.name as ongoing_val, yn3.name as is_review_val, 
+           yn4.name as reviewed_val
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
    LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
@@ -22,4 +23,8 @@ CREATE OR REPLACE VIEW kmdata.vw_Press AS
    LEFT JOIN kmdata.dmy_single_dates sd2 ON w.series_ended_on_dmy_single_date_id = sd2.id
    LEFT JOIN kmdata.dmy_single_dates sd3 ON w.series_started_on_dmy_single_date_id = sd3.id
    left join researchinview.riv_publication_freq sf on sf.id = cast(w.series_frequency as int)
-  WHERE w.work_type_id = 54;
+  left join researchinview.riv_single_series yn1 on yn1.id = w.single_or_series
+   left join researchinview.riv_yes_no yn2 on yn2.value = cast(w.series_ongoing as int)
+   left join researchinview.riv_yes_no yn3 on yn3.value = cast(w.is_review as int)
+   left join researchinview.riv_yes_no yn4 on yn4.value = w.peer_reviewed_ind
+  WHERE a.work_type_name = 'Press';
