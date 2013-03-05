9e519bfafa1d82da4115c7412bb22b6c7ee75af1
Joan Qin <qin.42@osu.edu>
2013-03-05 12:53:03 -0500

    update views

--- views/kmdata/kmdata.vw_Figure.sql
+++ views/kmdata/kmdata.vw_Figure.sql
@@ -1,8 +1,10 @@
  CREATE OR REPLACE VIEW vw_figure AS 
  SELECT a.id, a.resource_id, a.user_id, a.author_list, a.city, a.country, a.created_by, 
-	a.creation_dmy_single_date_id, sd2.month, sd2.year, a.isbn, a.journal_title, a.original_publication_type,
+	a.creation_dmy_single_date_id, sd2.month as creation_month, sd2.year as creation_year, 
+	a.isbn, a.journal_title, a.original_publication_type,
          b.name as originaltype_val, a.beginning_page, a.ending_page, a.percent_authorship, a.publication_type_id, 
-         c.name as type_val, a.publication_dmy_single_date_id, sd.month, sd.year, a.publisher, a.state, a.title, a.publication_title,  
+         c.name as type_val, a.publication_dmy_single_date_id, sd.month as publication_month, sd.year as publication_year, 
+         a.publisher, a.state, a.title, a.publication_title,  
          a.url, a.volume, a.work_type_id, wt.work_type_name, kn.narrative_text,
           a.created_at, a.updated_at, a.issuing_organization, al.is_public, al.is_active
    FROM kmdata.works a
