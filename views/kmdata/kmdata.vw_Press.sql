2f4c0501d5ff58f6ac33841b7476dd08f0efe0b4
Joan Qin <qin.42@osu.edu>
2013-03-05 09:22:10 -0500

    update views

--- views/kmdata/kmdata.vw_Press.sql
+++ views/kmdata/kmdata.vw_Press.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE VIEW kmdata.vw_Press AS 
+CREATE OR REPLACE VIEW kmdata.vw_Press AS 
  SELECT   w.id, w.resource_id, w.user_id,   w.article_title, w.author_list, w.is_review,
           w.issn, w.issue, w.beginning_page, w.ending_page, w.percent_authorship, w.frequency_of_publication, g.name as publication_frequency,
           w.frequency_of_publication_other, w.publication_title, w.publication_type_id, h.name as press_publication_type,
@@ -8,10 +8,13 @@
           w.series_frequency, w.series_frequency_other, w.series_ongoing, 
           w.series_started_on_dmy_single_date_id, sd3.year as series_start_year, sd3.month as series_start_month, sd3.day as series_start_day,
           w.title, w.single_or_series, w.url, w.volume, w.created_at, w.updated_at,  w.work_type_id, a.work_type_name, 
-           sd1.year as publicatin_year, sd1.month as publication_month, sd1.day as publication_day, b.narrative_text, c.is_public
+           sd1.year as publicatin_year, sd1.month as publication_month, sd1.day as publication_day, b.narrative_text, c.is_public,  c.is_active,
+           sf.name as series_frequency_name, yn1.name as single_series_val, yn2.name as ongoing_val, yn3.name as is_review_val, 
+           yn4.name as reviewed_val
    FROM kmdata.works w
    LEFT JOIN kmdata.dmy_single_dates sd1 ON w.publication_dmy_single_date_id = sd1.id
-   left join kmdata.narratives b on w.resource_id = b.resource_id
+   LEFT JOIN kmdata.work_narratives wn ON w.id = wn.work_id
+      LEFT JOIN kmdata.narratives b on wn.narrative_id = b.id
    left join researchinview.activity_import_log c on c.resource_id = w.resource_id
    left join kmdata.work_types a on a.id = w.work_type_id
    left join researchinview.riv_review_types f on f.id = w.review_type_id
@@ -19,7 +22,12 @@
    left join researchinview.riv_press_publication_types h on h.id = w.publication_type_id
    LEFT JOIN kmdata.dmy_single_dates sd2 ON w.series_ended_on_dmy_single_date_id = sd2.id
    LEFT JOIN kmdata.dmy_single_dates sd3 ON w.series_started_on_dmy_single_date_id = sd3.id
-  WHERE w.work_type_id = 25;
+   left join researchinview.riv_publication_freq sf on sf.id = cast(w.series_frequency as int)
+  left join researchinview.riv_single_series yn1 on yn1.id = w.single_or_series
+   left join researchinview.riv_yes_no yn2 on yn2.value = cast(w.series_ongoing as int)
+   left join researchinview.riv_yes_no yn3 on yn3.value = cast(w.is_review as int)
+   left join researchinview.riv_yes_no yn4 on yn4.value = w.peer_reviewed_ind
+  WHERE a.work_type_name = 'Press';
 
 ALTER TABLE kmdata.vw_press
   OWNER TO kmdata;
