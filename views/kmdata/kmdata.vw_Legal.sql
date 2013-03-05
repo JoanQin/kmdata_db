ae7607e4bab51b4786c69f1444c78e8ace1754cd
Joan Qin <qin.42@osu.edu>
2013-03-05 12:51:31 -0500

    update views

--- views/kmdata/kmdata.vw_Legal.sql
+++ views/kmdata/kmdata.vw_Legal.sql
@@ -1,7 +1,9 @@
 create or replay view vw_legal as 
-SELECT w.id, w.resource_id, w.user_id, w.enacted, c.name as enacted_val, w.enacted_on_dmy_single_date_id, sd1.month, sd1.year,
+SELECT w.id, w.resource_id, w.user_id, w.enacted, c.name as enacted_val, w.enacted_on_dmy_single_date_id, 
+	sd1.month as enacted_month, sd1.year as enacted_year,
 	w.issuing_organization, w.legal_number, w.percent_authorship, w.role_id, e.name as role_val, w.role_designator, w.sponsor,
-	w.submission_dmy_single_date_id, sd2.month, sd2.year, w.submitted_to, w.title, w.type_of_activity, d.name as activity_type_val, w.type_of_activity_other,
+	w.submission_dmy_single_date_id, sd2.month as submission_month, sd2.year as submission_year, 
+	w.submitted_to, w.title, w.type_of_activity, d.name as activity_type_val, w.type_of_activity_other,
 	w.url, w.work_type_id,  a.work_type_name, w.created_at, w.updated_at,  	         
            b.narrative_text, al.is_public,  al.is_active
    FROM kmdata.works w
