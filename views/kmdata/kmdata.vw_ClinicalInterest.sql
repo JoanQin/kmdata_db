4394ac1bca8a0826d611340c4246ffce1c1e2221
Joan Qin <qin.42@osu.edu>
2013-03-06 13:51:42 -0500

    add new views

---
+++ views/kmdata/kmdata.vw_ClinicalInterest.sql
@@ -0,0 +1,9 @@
+create or replace view vw_ClinicalInterest as 
+select  a.id, a.narrative_type_id, a.narrative_text, a.private_ind, a.created_at,
+	a.updated_at, a.resource_id, b.user_id, al.is_active, na.narrative_desc, 
+	kmd_dev_riv_user.getaamcmedicalspecialties(a.narrative_text) as medical_specialties
+from kmdata.narratives a
+left join kmdata.user_narratives b on a.id = b.narrative_id
+left join researchinview.activity_import_log al on al.resource_id = a.resource_id
+left join kmdata.narrative_types na on na.id = a.narrative_type_id
+where na.narrative_desc = 'Clinical Interest'
