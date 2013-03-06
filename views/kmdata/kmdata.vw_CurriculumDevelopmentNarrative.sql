138c93312c2500682b45475542095bc85f9ac1bd
Joan Qin <qin.42@osu.edu>
2013-03-06 14:02:32 -0500

    add new view

---
+++ views/kmdata/kmdata.vw_CurriculumDevelopmentNarrative.sql
@@ -0,0 +1,8 @@
+create or replace view vw_CurriculumDevelopmentNarrative as 
+select  a.id, a.narrative_type_id, a.narrative_text, a.private_ind, a.created_at,
+	a.updated_at, a.resource_id, b.user_id, al.is_active, na.narrative_desc
+from kmdata.narratives a
+left join kmdata.user_narratives b on a.id = b.narrative_id
+left join researchinview.activity_import_log al on al.resource_id = a.resource_id
+left join kmdata.narrative_types na on na.id = a.narrative_type_id
+where na.narrative_desc = 'Curriculum Development'
